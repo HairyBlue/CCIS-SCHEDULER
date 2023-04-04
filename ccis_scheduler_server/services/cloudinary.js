0557adde3cdf1f63c4590c3fc982824a77fbb403
Hairy Blue <pecision.nickimarty@gmail.com>
2023-04-04 09:29:42 +0800

    fix cloudinary folder directory

--- ccis_scheduler_server/services/cloudinary.js
+++ ccis_scheduler_server/services/cloudinary.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
-        folder: 'CCIS SCHEDULER',
-        allowedFormats: ['jpeg', 'png', 'jpg']
+    params: {
+     folder: 'CCIS SCHEDULER',
+     allowedFormats: ['jpeg', 'png', 'jpg']
+    }
 });
 
 module.exports = {
