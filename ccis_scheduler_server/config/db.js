c2510c3e83a64c07248346c74cfd91cbaac8ddf4
Hairy Blue <pecision.nickimarty@gmail.com>
2023-04-06 14:38:40 +0800

    commit change

--- ccis_scheduler_server/config/db.js
+++ ccis_scheduler_server/config/db.js
@@ -6,7 +6,10 @@ const db = mysql.createPool({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PWD,
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+      }
 }).promise()
 
 module.exports = db;
